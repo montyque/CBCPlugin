d754e30ce8b581cf55af6ad41d3a3a936db4e1d1
Euan Monteclaro <emonteclaro.plasma@gmail.com>
2026-08-01 13:49:19 +1200

    Migrated TagMapData to use TeamRequirements

--- src/main/java/neonique/cbcplugin_new/gamemodes/crossbowtag/TagMapData.java
+++ src/main/java/neonique/cbcplugin_new/gamemodes/crossbowtag/TagMapData.java
@@ -1,6 +1,6 @@
 package neonique.cbcplugin_new.gamemodes.crossbowtag;
 
-import neonique.cbcplugin_new.core.TeamColor;
+import neonique.cbcplugin_new.mapconfig.TeamRequirements;
 import neonique.cbcplugin_new.mapconfig.spawns.TeamSpawnList;
 import neonique.cbcplugin_new.mapconfig.CBCMap;
 import neonique.cbcplugin_new.mapconfig.TeamMapData;
@@ -8,39 +8,30 @@ import neonique.cbcplugin_new.util.ConfigUtil;
 import org.bukkit.configuration.Configuration;
 import org.bukkit.configuration.ConfigurationSection;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 public record TagMapData (CBCMap map,
-                          int minTeams,
-                          int maxTeams,
-                          List<TeamColor> validTeamColors,
+                          TeamRequirements teamRequirements,
                           TeamSpawnList taggerSpawnList,
                           TeamSpawnList evaderSpawnList,
                           boolean evadersFrozenOnSetup) implements TeamMapData {
 
     public static TagMapData fromConfig (CBCMap map, Configuration gamemodeConfig) {
 
-        int minTeams = ConfigUtil.requireInt(gamemodeConfig, "min_teams");
-        int maxTeams = ConfigUtil.requireInt(gamemodeConfig, "max_teams");
-        List<TeamColor> validTeamColors = ConfigUtil.requireStringList(gamemodeConfig, "valid_team_colors").stream()
-                .map(s -> TeamColor.valueOf(s.toUpperCase()))
-                .collect(Collectors.toList());
+        TeamRequirements req = TeamRequirements.fromConfig(gamemodeConfig);
 
         ConfigurationSection taggerSpawnSection = ConfigUtil.requireConfigurationSection(gamemodeConfig, "tagger_spawns");
         TeamSpawnList taggerSpawnList = TeamSpawnList.fromConfig(taggerSpawnSection,
-                validTeamColors,
-                maxTeams);
+                req.validTeamColors(),
+                req.maxTeams());
 
         ConfigurationSection evaderSpawnSection = ConfigUtil.requireConfigurationSection(gamemodeConfig, "evader_spawns");
         TeamSpawnList evaderSpawnList = TeamSpawnList.fromConfig(evaderSpawnSection,
-                validTeamColors,
-                maxTeams);
+                req.validTeamColors(),
+                req.maxTeams());
 
         boolean evadersFrozenOnSetup = ConfigUtil.getBoolean(gamemodeConfig, "evaders_frozen_on_setup")
                 .orElse(true);
 
-        return new TagMapData(map, minTeams, maxTeams, validTeamColors,
+        return new TagMapData(map, req,
                 taggerSpawnList, evaderSpawnList, evadersFrozenOnSetup);
 
     }
