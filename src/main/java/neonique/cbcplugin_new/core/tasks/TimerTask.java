037176657474bfba98b018478f034a944831e808
Euan Monteclaro <emonteclaro.plasma@gmail.com>
2026-08-06 23:31:48 +1200

    Modified TimerTask for more general use
    Added timerEventsMap that can be overridden to fire an event at certain timestamps
    Added run condition that terminates the timer

--- src/main/java/neonique/cbcplugin_new/core/tasks/TimerTask.java
+++ src/main/java/neonique/cbcplugin_new/core/tasks/TimerTask.java
@@ -4,28 +4,50 @@ import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.Map;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 public class TimerTask extends BukkitRunnable {
 
     private final Consumer<Integer> onTick;
+    private final Supplier<Boolean> runCondition;
     private final Runnable onEnd;
-    private final Map<Integer, Consumer<Integer>> onTimerEquals;
 
     private int secs;
 
-    public TimerTask (Consumer<Integer> onTick,
-                      Runnable onEnd,
-                      Map<Integer, Consumer<Integer>> onTimerEquals) {
+    public TimerTask (int length,
+                      Consumer<Integer> onTick,
+                      Supplier<Boolean> runCondition,
+                      Runnable onEnd) {
+        this.secs = length;
         this.onTick = onTick;
+        this.runCondition = runCondition;
         this.onEnd = onEnd;
-        this.onTimerEquals = onTimerEquals;
+    }
+
+    public TimerTask (int length,
+                      Consumer<Integer> onTick,
+                      Runnable onEnd) {
+        this(length, onTick, () -> true, onEnd);
     }
 
     @Override
     public void run () {
 
+        if (!runCondition.get()) {
+            this.cancel();
+        }
+
         secs--;
+
+        // Run any task scheduled when the timer hits this value
+        if (timerEventsMap().containsKey(secs)) {
+            timerEventsMap().get(secs).accept(secs);
+        }
+
+        // Run a task scheduled to occur every trip
         onTick.accept(secs);
+
+        // Run a task when the timer hits 0, and cancel if still 0 after
         if (secs == 0) {
             onEnd.run();
             if (secs == 0) this.cancel();
@@ -39,6 +61,10 @@ public class TimerTask extends BukkitRunnable {
 
     public void setSecs (int i) {
         this.secs = i;
+    }
+
+    public Map<Integer, Consumer<Integer>> timerEventsMap () {
+        return Map.of();
     }
 
 }
