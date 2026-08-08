26d2479243f764f7f721a0ee64bebb77532818fb
Euan Monteclaro <emonteclaro.plasma@gmail.com>
2026-08-08 16:29:51 +1200

    Fixed errors and removed unrequired classes

--- src/main/java/neonique/cbcplugin_new/weapons/CreeperCannon.java
+++ src/main/java/neonique/cbcplugin_new/weapons/CreeperCannon.java
@@ -1,9 +1,7 @@
 package neonique.cbcplugin_new.weapons;
 
 import neonique.cbcplugin_new.CBCPlugin;
-import neonique.cbcplugin_new.combat.ProjectileManager;
 import neonique.cbcplugin_new.weapons.presets.CreeperCannonSettings;
-import neonique.cbcplugin_new.weapons.presets.CreeperPreset;
 import neonique.cbcplugin_new.core.CBCPlayer;
 import neonique.cbcplugin_new.weapons.projectiles.CBCCreeper;
 import neonique.cbcplugin_new.weapons.projectiles.Projectile;
