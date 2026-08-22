165e98118226a645921f1e5c037ebe21bc07ba89
Euan Monteclaro <emonteclaro.plasma@gmail.com>
2026-08-22 21:55:26 +1200

    Added join, leave and stop commands to practice manager

--- src/main/java/neonique/cbcplugin_new/commands/PracticeCommand.java
+++ src/main/java/neonique/cbcplugin_new/commands/PracticeCommand.java
@@ -1,10 +1,15 @@
 package neonique.cbcplugin_new.commands;
 
 import dev.jorel.commandapi.CommandAPICommand;
-import neonique.cbcplugin_new.commands.practice.PracticeStartCommand;
+import dev.jorel.commandapi.CommandPermission;
+import dev.jorel.commandapi.executors.CommandArguments;
+import neonique.cbcplugin_new.commands.arguments.MapDataArgumentParser;
+import neonique.cbcplugin_new.mapconfig.CBCMapData;
 import neonique.cbcplugin_new.practice.PracticeManager;
-
-import java.util.List;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class PracticeCommand implements Subcommand {
 
@@ -17,7 +22,56 @@ public class PracticeCommand implements Subcommand {
     @Override
     public CommandAPICommand get() {
         return new CommandAPICommand("practice")
-                .withSubcommand(new PracticeStartCommand(practiceManager).get());
+
+                .withSubcommand(new CommandAPICommand("start")
+                        .withPermission(CommandPermission.OP)
+                        .withRequirement(s -> !practiceManager.instanceActive())
+                        .withArguments(
+                                MapDataArgumentParser.argument("mapId", practiceManager::getPracticeMaps)
+                        )
+                        .executes(this::startExecutor))
+
+                .withSubcommand(new CommandAPICommand("stop")
+                        .withPermission(CommandPermission.OP)
+                        .withRequirement(s -> practiceManager.instanceActive())
+                        .executes(this::stopExecutor))
+
+                .withSubcommand(new CommandAPICommand("join")
+                        .withRequirement(s -> practiceManager.instanceActive())
+                        .withRequirement(s -> !practiceManager.instanceHasPlayer((Player) s))
+                        .executesPlayer(this::joinExecutor))
+
+                .withSubcommand(new CommandAPICommand("leave")
+                        .withRequirement(s -> practiceManager.instanceActive())
+                        .withRequirement(s -> practiceManager.instanceHasPlayer((Player) s))
+                        .executesPlayer(this::leaveExecutor));
+
+    }
+
+    private void startExecutor (CommandSender sender, CommandArguments arguments) {
+
+        CBCMapData map = (CBCMapData) arguments.get("mapId");
+        practiceManager.newInstance(map);
+
+        sender.sendMessage(Component.text("Opened the practice arena.").color(NamedTextColor.YELLOW));
+
+    }
+
+    private void stopExecutor (CommandSender sender, CommandArguments arguments) {
+
+        practiceManager.endInstance();
+        sender.sendMessage(Component.text("Closed the practice arena.").color(NamedTextColor.YELLOW));
+
+    }
+
+    private void joinExecutor (CommandSender sender, CommandArguments arguments) {
+        Player player = (Player) sender;
+        practiceManager.playerJoin(player);
+    }
+
+    private void leaveExecutor (CommandSender sender, CommandArguments arguments) {
+        Player player = (Player) sender;
+        practiceManager.playerLeave(player);
     }
 
 }
